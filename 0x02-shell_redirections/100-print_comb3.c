39da3b8880dadfe76301bb08919963d01cf73c7e
swekenyi <sekidy20@gmail.com>
2023-03-20 02:41:09 -0700

    prints all possible different combinations of two digits

--- 0x02-shell_redirections/100-print_comb3.c
+++ 0x02-shell_redirections/100-print_comb3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints all possible combinations of two different digits
+ * main - Prints all possible combinations of two different digits.
  *
  * 
  * Return: Always 0.
